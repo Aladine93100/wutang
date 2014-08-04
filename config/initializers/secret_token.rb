644fde5db29e7aca5e9ab77b5c9f99e1fd67a307
Aladine <alazaiane@yahoo.fr>
2014-08-04 09:22:30 +0200

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wutang::Application.config.secret_key_base = 'e0a6037a163c5a62ded84baf5bbd4e01ae7f121a220f8ac5984c97c62cfa7c467adb22e2231dda83cb06e0b4a5220dfde3f150ca1a4c64f897b3510d67b16bb4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wutang::Application.config.session_store :cookie_store, key: '_wutang_session'
